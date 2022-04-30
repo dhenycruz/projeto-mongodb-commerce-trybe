a333c43fcf70329868174b0acc21fa22046a5ba4
Dheniarley Cruz <dheniarley.ds@gamil.com>
2022-04-30 12:10:15 -0300

    styles: fix indentation errors 3.

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -2,8 +2,8 @@ db.produtos.find(
   {
     $or: [
       { curtidas: 36 },
-      { vendidos: 85 }
-    ]
+      { vendidos: 85 },
+    ],
   },
   { _id: 0, nome: 1, curtidas: 1, vendidos: 1 },
 );
